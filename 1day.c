573bc99067f62c9c49c088847afc60b7bee201d4
raina901 <rainabansal.wp123@gmail.com>
2026-08-10 20:43:30 +0530

    Upload fresh edited version

---
+++ 1day.c
@@ -0,0 +1,32 @@
+Q1: Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The sum of %d and %d is: %d\n",a,b,sum);
+    return 0;
+}
+
+Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include<stdio.h>
+int main()
+{ 
+    int a,b,sum,diff,prod,quot;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    sum=a+b;
+    diff=a-b;
+    prod=a*b;
+    quot=a/b;
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n",sum,diff,prod,quot);
+    return 0;
+}
