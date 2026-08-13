b7f1efc8ae9b54f84ba9328bde7de31778294b2f
raina901 <rainabansal.wp123@gmail.com>
2026-08-13 21:41:03 +0530

    Add 4day1 and 4day2 to c-programming repo

---
+++ 4day1.c
@@ -0,0 +1,15 @@
+Q7: Write a program to swap two numbers without using a third variable.
+
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the two numbers a and b:");
+    scanf("%d %d", &a, &b);
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swap: a = %d, b = %d\n", a, b);
+    return 0;
+}
