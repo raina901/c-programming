46a13d13c7a3c97d28a1669d1ed3963a1794037b
raina901 <rainabansal.wp123@gmail.com>
2026-08-14 21:26:12 +0530

    Add 5day2.c to c-programming repo

---
+++ 5day2.c
@@ -0,0 +1,17 @@
+Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+int main() 
+{
+    int totalSeconds, hours, minutes, seconds;git
+
+    printf("Enter time in seconds: ");
+    scanf("%d", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    printf("Time in hours:minutes:seconds format is %02d:%02d:%02d\n", hours, minutes, seconds);
+    return 0;
+}
