0a9502b019049b0778c5781745e8ceb547e03aed
raina901 <rainabansal.wp123@gmail.com>
2026-08-28 19:26:47 +0530

    Add files 19day1.c and 19day2.c to c-programming repo

---
+++ 19day2.c
@@ -0,0 +1,20 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+#include<stdio.h>
+int main ()
+{
+    int num, sum = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    while(num != 0)
+    {
+        digit = num % 10;
+        sum += digit;     
+        num /= 10;       
+    }
+    
+    printf("Sum of digits is: %d", sum);
+    
+    return 0;
+}
