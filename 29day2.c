f9dc41130ecbb1cbe866efbbb32804df10fc68d7
raina901 <rainabansal.wp123@gmail.com>
2026-09-07 01:09:09 +0530

    Add files 29day1 and 29day2 to c-programming repo

---
+++ 29day2.c
@@ -0,0 +1,31 @@
+//Q58: Find the maximum and minimum element in an array.
+
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int max = arr[0];
+    int min = arr[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    
+    printf("Max=%d, Min=%d\n", max, min);
+    
+    return 0;
+}
