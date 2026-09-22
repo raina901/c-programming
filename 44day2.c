2c564bfe3f2f90365f2a1913df88e031129f5a4a
raina901 <rainabansal.wp123@gmail.com>
2026-09-22 21:53:29 +0530

    Add files 44day1 and 44day2 to c-programming repo

---
+++ 44day2.c
@@ -0,0 +1,17 @@
+//Q88: Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%[^\n]", str); 
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("Modified string: %s\n", str);
+    return 0;
+}
